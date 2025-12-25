e29d8febf92817f4caa083ed71d971113d45c5e1
davecrab <65996799+davecrab@users.noreply.github.com>
2025-12-25 12:56:49 -0800

    Initial Commit

---
+++ MusicAssistant/MusicAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAssistantApp.swift
+//  MusicAssistant
+//
+//  Created by Dave Crabtree on 2025-12-25.
+//
+
+import SwiftUI
+
+@main
+struct MusicAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicAssistantTests/MusicAssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAssistantTests.swift
+//  MusicAssistantTests
+//
+//  Created by Dave Crabtree on 2025-12-25.
+//
+
+import Testing
+@testable import MusicAssistant
+
+struct MusicAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
